4cd0d6db934489f471497b58501111858fa452eb
Arei <98773616+Arei1997@users.noreply.github.com>
2022-11-30 13:03:12 +0000

    Add files via upload

---
+++ login-code/create_auction_result.php
@@ -0,0 +1,53 @@
+<?php include_once("header.php")?>
+<?php include_once("include/checkauth.php"); ?>
+<div class="container my-5">
+
+<?php
+
+// This function takes the form data and adds the new auction to the database.
+
+/* TODO #1: Connect to MySQL database (perhaps by requiring a file that
+            already does this). */
+
+
+/* TODO #2: Extract form data into variables. Because the form was a 'post'
+            form, its data can be accessed via $POST['auctionTitle'], 
+            $POST['auctionDetails'], etc. Perform checking on the data to
+            make sure it can be inserted into the database. If there is an
+            issue, give some semi-helpful feedback to user. */
+
+
+/* TODO #3: If everything looks good, make the appropriate call to insert
+            data into the database. */
+            
+
+// If all is successful, let user know.
+
+$txtTitle = $_POST['txtTitle'];
+$txtDetails = $_POST['txtDetails'];
+$ddCategory = $_POST['ddCategory'];
+$txtStartPrice = $_POST['txtStartPrice'];
+$txtReservePrice = $_POST['txtReservePrice'];
+$txtEndDate = $_POST['txtEndDate'];
+$user_id = $_SESSION['user']['id'];
+$date = date('Y-m-d H:i:s');
+
+$insertQuery = "insert into tbl_listings(title,details,category_id,starting_price,reserve_price,end_date,user_id,is_active,created_at)
+values('$txtTitle','$txtDetails',$ddCategory,'$txtStartPrice','$txtReservePrice','$txtEndDate',$user_id, 1, '$date')";
+
+$insert = $mysqli->query($insertQuery);
+
+if($insert){
+    echo('<div class="text-center">Auction successfully created! <a href="'.$base_url.'/mylistings.php">View your new listing.</a></div>');
+}else{
+    echo('<div class="text-center">Something went wrong! <a href="'.$base_url.'/create_auction.php">Create Again</a></div>');
+}
+
+
+
+?>
+
+</div>
+
+
+<?php include_once("footer.php")?>
---
+++ login-code/place_bid.php
@@ -0,0 +1,7 @@
+<?php
+include_once("header.php");
+include_once("include/checkauth.php");
+// TODO: Extract $_POST variables, check they're OK, and attempt to make a bid.
+// Notify user of success/failure and redirect/give navigation options.
+
+?>
