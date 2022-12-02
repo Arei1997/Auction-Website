524a5f05fdfb97a86400c93aedfd4303fdb3e326
Alex000311 <116173111+Alex000311@users.noreply.github.com>
2022-12-02 22:28:14 +0000

    Add files via upload

---
+++ BidingFinish/process_registration.php
@@ -0,0 +1,8 @@
+<?php 
+include_once("header.php");
+
+// TODO: Extract $_POST variables, check they're OK, and attempt to create
+// an account. Notify user of success/failure and redirect/give navigation 
+// options.
+
+?>
