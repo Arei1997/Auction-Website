5a9d33ceeb49fe72faadd6f94368effec5705440
JunmingHuJim <114617873+JunmingHuJim@users.noreply.github.com>
2022-12-02 22:45:14 +0000

    Add files via upload

---
+++ recommendation/mylistings.php
@@ -0,0 +1,57 @@
+<?php include_once("header.php")?>
+<?php require("utilities.php")?>
+<?php include_once("include/checkauth.php"); ?>
+<div class="container">
+
+<h2 class="my-3">My listings</h2>
+
+
+<div class="container mt-5">
+
+<!-- TODO: If result set is empty, print an informative message. Otherwise... -->
+
+<ul class="list-group">
+
+<!-- TODO: Use a while loop to print a list item for each auction listing
+     retrieved from the query -->
+
+<?php
+
+  $user_id = $_SESSION['user']['id'];
+  $getUserListingQuery = "select * from tbl_listings where user_id = $user_id";
+  $listings = $mysqli->query($getUserListingQuery);
+  
+  // Demonstration of what listings will look like using dummy data.
+  while($row = mysqli_fetch_array($listings)){
+    $listingone = $row['listing_id'];
+
+    $getBidingNumQuery = "SELECT COUNT(*) from biding WHERE listing_id = $listingone";
+    $BidingNum = $mysqli->query($getBidingNumQuery);   
+    $BidingNumRow = mysqli_fetch_array($BidingNum);
+    
+    
+    print_listing_li($row['listing_id'], $row['title'], $row['details'], $row['starting_price'], $BidingNumRow[0], date_create($row['end_date']));
+  }
+  
+  
+?>
+
+</ul>
+
+<?php
+  // This page is for showing a user the auction listings they've made.
+  // It will be pretty similar to browse.php, except there is no search bar.
+  // This can be started after browse.php is working with a database.
+  // Feel free to extract out useful functions from browse.php and put them in
+  // the shared "utilities.php" where they can be shared by multiple files.
+  
+  
+  // TODO: Check user's credentials (cookie/session).
+  
+  // TODO: Perform a query to pull up their auctions.
+  
+  // TODO: Loop through results and print them out as list items.
+  
+?>
+
+<?php include_once("footer.php")?>
