989b338209adadf9dcdbc0598f2027f97f7daabe
Arei <98773616+Arei1997@users.noreply.github.com>
2022-12-07 15:24:42 +0000

    Delete TEST directory

--- TEST/login-code/include/checkauth.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-if(!isset($_SESSION['user'])){
-    header("Location: $base_url/register.php");   
-}
--- TEST/login-code/place_bid.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once("header.php");
-include_once("include/checkauth.php");
-// TODO: Extract $_POST variables, check they're OK, and attempt to make a bid.
-// Notify user of success/failure and redirect/give navigation options.
-
-?>
