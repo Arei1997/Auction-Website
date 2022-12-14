0ee55b0efe738b8b8b586ea6803a7c19189968c0
JunmingHuJim <114617873+JunmingHuJim@users.noreply.github.com>
2022-12-14 15:06:37 +0000

    Add files via upload

--- FinalTest(Hope_it_is)/watchlist_notif.php
+++ FinalTest(Hope_it_is)/watchlist_notif.php
@@ -10,41 +10,42 @@ require 'PHPMailer/SMTP.php';
 ?>
 <?php
 function smtpmailer($to, $from, $from_name, $subject, $body)
+{
+
+    $mail = new PHPMailer();
+
+    /* Tells PHPMailer to use SMTP. */
+    $mail->isSMTP();
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    $mail->SMTPAuth   = TRUE;
+    $mail->SMTPSecure = "ssl"; // 使用ssl
+    /* SMTP parameters. */
+    $mail->Port       = 465; // 端口改成465
+    $mail->Host       = "smtp.qq.com";
+    $mail->Username   = "978338509@qq.com";
+    $mail->Password   = "snywkqllddzzbfda";
+    $mail->isHTML(true);
+    /* Add a recipient. */
+    $mail->addAddress($to, 'User');
+    /* Set the mail sender. */
+    $mail->setFrom($from, $from_name);
+    /* Set the subject. */
+    $mail->Subject = $subject;
+    /* Set the mail message body. */
+    $mail->Body = $body;
+    /* Finally send the mail. */
+
+    if(!$mail->Send())
     {
-
-			$mail = new PHPMailer;
-
-			/* Tells PHPMailer to use SMTP. */
-			$mail->isSMTP();
-			#$mail->SMTPDebug = SMTP::DEBUG_SERVER;
-			$mail->SMTPAuth   = TRUE;
-			$mail->SMTPSecure = "ssl"; // 使用ssl
-			/* SMTP parameters. */
-			$mail->Port       = 465; // 端口改成456
-			$mail->Host       = "smtp.qq.com";
-			$mail->Username   = "978338509@qq.com"; 
-			$mail->Password   = "imfsscrmdkcqbeci";
-			$mail->isHTML(true);
-			/* Add a recipient. */
-			$mail->addAddress($to, 'User');
-			/* Set the mail sender. */
-			$mail->setFrom($from, $from_name);
-			/* Set the subject. */
-			$mail->Subject = $subject;
-			/* Set the mail message body. */
-			$mail->Body = $body;
-			/* Finally send the mail. */
-			if(!$mail->Send())
-			{
-					$error ="Email failed to send.";
-					return $error;
-			}
-			else
-			{
-					$error = "Thanks You !! Your email is sent.";
-					return $error;
-			}
-	}
+        $error ="Email failed to send.";
+        return $error;
+    }
+    else
+    {
+        $error = "Thanks You !! Your email is sent.";
+        return $error;
+    }
+}
 
 if (isset($_SESSION['user'])){
   $listing_id = $_SESSION['curitem_id'];
@@ -79,7 +80,7 @@ $buyer_notified = 0;
 
 if ($buyer_email[0] == $outbid_email[0]) {
 	$name = "Auction administrator"; //sender’s name
-	$from = "comp0178group3@gmail.com"; //sender’s e mail address
+	$from = "978338509@qq.com"; //sender’s e mail address
 	$recipient = "$buyer_email[0]"; //recipient
 	$mail_body = "This is an email confirming that you have bid £$bid_price on the auction called '$listing_title[0]'"; //mail body
 	$subject = "Bid confirmation"; //subject
@@ -88,7 +89,7 @@ if ($buyer_email[0] == $outbid_email[0]) {
 }
 else {
 	$name = "Auction administrator"; //sender’s name
-	$from = "comp0178group3@gmail.com"; //sender’s e mail address
+	$from = "978338509@qq.com"; //sender’s e mail address
 	$recipient = "$outbid_email[0]"; //recipient
 	$mail_body = "Your bid of £$previous_top_bid on the auction called '$listing_title[0]' has been outbid. The current highest bid is now £$bid_price"; //mail body
 	$subject = "Outbid notification"; //subject
@@ -107,7 +108,7 @@ while ($row = mysqli_fetch_array($watchlist_result))
 
 	if ($row[0] == $buyer_email[0]) {
 		$name = "Auction administrator"; //sender’s name
-		$from = "comp0178group3@gmail.com"; //sender’s e mail address
+		$from = "978338509@qq.com"; //sender’s e mail address
 		$recipient = "$buyer_email[0]"; //recipient
 		$mail_body = "This is an email confirming that you have bid £$bid_price on the auction called '$listing_title[0]'"; //mail body
 		$subject = "Bid confirmation"; //subject
@@ -115,7 +116,7 @@ while ($row = mysqli_fetch_array($watchlist_result))
 	}
 	else {
 		$name = "Happy Auction House"; //sender’s name
-		$from = "comp0178group3@gmail.com"; //sender’s e mail address
+		$from = "978338509@qq.com"; //sender’s e mail address
 		$recipient = "$row[0]"; //recipient
 		$mail_body = "The auction called '$listing_title[0]' which you are watching just received a bid for £$bid_price "; //mail body
 		$subject = "Auction you are watching"; //subject
